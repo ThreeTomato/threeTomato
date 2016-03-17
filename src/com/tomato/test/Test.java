86e3331d3c458d70ee262cb211e96e8032aaae65
iniestano8 <446035779@qq.com>
2016-03-17 21:31:01 +0800

    九九乘法表提交

--- src/com/tomato/test/Test.java
+++ src/com/tomato/test/Test.java
@@ -2,4 +2,14 @@ package com.tomato.test;
 
 public class Test {
 
+	public static void main(String[] agrs){
+		int i;int j;
+		System.out.println("============华丽丽的分割线==================");
+		for (i=1;i<=9;i++){
+			for (j=1;j<=i;j++){
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+			}
+			System.out.println();
+		}
+	}
 }
